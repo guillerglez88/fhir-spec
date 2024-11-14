94caae7c6783046f25a9d2fb0f4db22f265d3bc3
Guillermo <guillerglez88@gmail.com>
2024-11-14 21:41:22 +0100

    refactoring gen primitives

--- src/fhir_spec/core.clj
+++ src/fhir_spec/core.clj
@@ -1,29 +1,40 @@
 (ns fhir-spec.core
   (:require
-   [clojure.pprint :as pp]
    [clojure.java.io :as io]
    [fhir-spec.config :refer [config]]
-   [fhir-spec.artifacts :refer [definitions]]
-   [fhir-spec.gen :refer [primitive]]))
+   [fhir-spec.artifacts :refer [definitions download-spec!]]
+   [fhir-spec.gen :as gen]))
 
 (def definitions-by-kind
   (delay
-    (->> (:entry (definitions "tmp/profiles-types.json"))
+    (->> (:entry (definitions (str (:base-dir config) "/profiles-types.json")))
          (map :resource)
          (group-by :kind))))
 
+(defn write [wx lines]
+  (doseq [line lines]
+    (if (string? line)
+      (.write wx line)
+      (.write wx (str line)))
+    (.newLine wx)))
+
 (defn write-primitives []
-  (with-open [wx (io/writer (io/file (str (:base-dir config) "/primitives.clj")) :append true)]
-    (pp/pprint '(ns primitives (:require [clojure.spec.alpha :as s])) wx)
-    (doseq [definition (get @definitions-by-kind "primitive-type")]
+  (let [f (io/file (str (:base-dir config) "/primitives.clj"))]
+    (io/delete-file f true)
+    (with-open [wx (io/writer f :append true)]
+      (write wx [(gen/primitives-ns)])
       (.newLine wx)
-      (.write wx (format ";; %s \n" (:url definition)))
-      (pp/pprint (primitive definition) wx))))
+      (doseq [definition (get @definitions-by-kind "primitive-type")]
+        (write wx (gen/primitive definition))
+        (.newLine wx)))))
 
 (comment
 
+  (download-spec!)
+
   (write-primitives)
 
-  (first (get @definitions-by-kind "complex-type"))
+  ;; CodeableConcept: #9
+  (nth (get @definitions-by-kind "complex-type") 9)
 
   :.)
--- src/fhir_spec/gen.clj
+++ src/fhir_spec/gen.clj
@@ -1,24 +1,37 @@
 (ns fhir-spec.gen)
 
 (def native-primitives
-  {:fhir/boolean 'string?
-   :fhir/decimal 'decimal?
-   :fhir/integer 'integer?
-   :fhir/integer64 'integer?
-   :fhir/positiveInt 'pos-int?
-   :fhir/string 'string?
-   :fhir/unsignedInt 'pos-int?
-   :fhir/xhtml 'string?})
+  {"boolean" 'boolean?
+   "decimal" 'number?
+   "integer" 'integer?
+   "integer64" 'integer?
+   "positiveInt" 'pos-int?
+   "string" 'string?
+   "unsignedInt" 'pos-int?})
+
+(defn primitives-ns []
+  '(ns primitives
+     (:require [clojure.spec.alpha :as s])))
 
 (defn primitive [definition]
-  (let [sym (symbol (str ":fhir/" (:name definition)))
-        regex (->> (get-in definition [:snapshot :element])
-                   (mapcat :type)
-                   (mapcat :extension)
-                   (filter (comp #{"http://hl7.org/fhir/StructureDefinition/regex"} :url))
-                   (some (fn [{:keys [valueString]}]
-                           (read-string (str "#\"" valueString "\"")))))]
-    (list 's/def sym
-          (or
-           (get native-primitives (read-string (str sym)))
-           (list 's/and 'string? (list 'partial 're-matches regex))))))
+  (letfn [(sd-val [sd]
+            (->> (get-in sd [:snapshot :element])
+                 (filter (comp #{(str (:name sd) ".value")} :id))
+                 (first)))
+          (val-type [val]
+            (->> (:type val)
+                 (mapcat :extension)
+                 (filter (comp #{"http://hl7.org/fhir/StructureDefinition/structuredefinition-fhir-type"} :url))
+                 (some :valueUrl)))
+          (val-regex [val]
+            (->> (:type val)
+                 (mapcat :extension)
+                 (filter (comp #{"http://hl7.org/fhir/StructureDefinition/regex"} :url))
+                 (some (fn [{:keys [valueString]}]
+                         (read-string (str "#\"" valueString "\""))))))]
+    (let [value (sd-val definition)
+          sym (symbol (str ":fhir/" (:name definition)))]
+      [(format ";; %s" (:short value))
+       (format ";; %s" (:url definition))
+       (list 's/def sym (or (get native-primitives (val-type value))
+                            (list 's/and 'string? (list 'partial 're-matches (val-regex value)))))])))
